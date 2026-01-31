e3f5e9b0a633a1555954e8651daf0381d0954394
zawmoewin120298-collab <zawmoewin120298@gmail.com>
2026-01-31 19:50:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ sleep 5
 
 echo "Starting Cloudflare Tunnel..."
 # သင့်ရဲ့ Token ကို ဒီနေရာမှာ တိုက်ရိုက်ထည့်ပေးထားပါတယ်
-/usr/local/bin/cloudflared tunnel --no-autoupdate run --token eyJhIjoiOGQxZmJmOGJjOWM1NmFkOGYyMjZhMGM1MmNlZjgxNzIiLCJ0IjoiYWY4YzMwNWQtOWEwMC00YWEwLWFjNjMtNDVlY2Q2YjkwYjAyIiwicyI6IlpERmlNak0zTXpndE9UWTFOaTAwWm1ZNExXSmlOekl0TmpkalltUmpOV0ZoT1dObCJ9
+/usr/local/bin/cloudflared tunnel --no-autoupdate run --token eyJhIjoiOGQxZmJmOGJjOWM1NmFkOGYyMjZhMGM1MmNlZjgxNzIiLCJ0IjoiNzUxZDRmNTctMzZmMC00ODMwLTlmMWItNjc5ZTI0NzlmMzc3IiwicyI6Ik1XVXlPR0pqTW1RdE5HTm1NeTAwTjJGaExXRTFZemd0TkRsa05HUTJZVGs1TUdWayJ9
